30ac2aa6f60ebda65067ebf949efcd78631c28aa
Gustavo.R <grosario015@gmail.com>
2023-10-22 21:05:09 -0400

    nueva dto
    Se agrega la dto turno_generado

---
+++ pj/aplication/dto/turno_generado.py
@@ -0,0 +1,9 @@
+import datetime as dt
+
+class turno_generado():
+      turno:str
+      estado_id: int
+      estacion_id: int
+      fecha_registro:dt.date
+      hora_registro:str
+      activo:bool
