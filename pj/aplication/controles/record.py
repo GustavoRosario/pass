ef05b094dc8ef413703201b629c766e3f4109f26
Gustavo.R <grosario015@gmail.com>
2023-10-25 19:01:50 -0400

    Reference Change
    Reference Change

--- pj/aplication/controles/record.py
+++ pj/aplication/controles/record.py
@@ -1,8 +1,8 @@
 from aplication.models import historical_record
-from aplication.dto.historial import record 
+from aplication.dto.dto_record import dto_record 
 import datetime as dt
 
-def register(_record:record):
+def register(_record:dto_record):
     historical = historical_record()
     historical.registration_date = dt.date.today()
     historical.registration_time = dt.datetime.now().strftime('%H:%M:%S')
