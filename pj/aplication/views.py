daed223bbaa1c530942abc26349ceaa7b9f656d3
Gustavo.R <grosario015@gmail.com>
2023-10-25 20:20:21 -0400

    Change name
    Change variable name.

--- pj/aplication/views.py
+++ pj/aplication/views.py
@@ -8,11 +8,11 @@ def next(request):
     
     _record = dto_record() #Objeto Historial
     _generated_turnn = dto_generated_turn()
-    _turno = turn.generate(2)
-    _record.turn = _turno
-    _generated_turnn.turn = _turno
+    _turn = turn.generate(2)
+    _record.turn = _turn
+    _generated_turnn.turn = _turn
     turn.register(_generated_turnn)
     record.register(_record)
-    html:str = '<html><body> <p><h1> <center><br>' + _turno +'</br></center><h1></p> </body></html>'
+    html:str = '<html><body> <p><h1> <center><br>' + _turn +'</br></center><h1></p> </body></html>'
     return HttpResponse(html)
     #return render(request,"Resultado:" + '', {})
