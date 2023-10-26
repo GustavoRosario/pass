dedca50b8acfbf16851a1e9dffa4e2aee01d5c94
Gustavo.R <grosario015@gmail.com>
2023-10-25 20:00:04 -0400

    Change names.
    Change names.

--- pj/aplication/views.py
+++ pj/aplication/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from aplication.controles import turno, historico
-from aplication.dto.historial import historial 
+from aplication.controles import turn, record
+from aplication.dto.dto_record import dto_record
+from aplication.dto.dto_generated_turn import dto_generated_turn
 
 def turnos(request):
     
-    hiobj = historial() #Objeto Historial
-    x = turno.generar(7)
-    hiobj.turno = x
-    historico.registrar(hiobj)
-
-    return HttpResponse(x)
+    _record = dto_record() #Objeto Historial
+    _generated_turnn = dto_generated_turn()
+    _turno = turn.generate(1)
+    _record.turn = _turno
+    _generated_turnn.turn = _turno
+    turn.register(_generated_turnn)
+    record.register(_record)
+    html:str = '<html><body> <p><h1> <center><br>' + _turno +'</br></center><h1></p> </body></html>'
+    return HttpResponse(html)
     #return render(request,"Resultado:" + '', {})
+    
