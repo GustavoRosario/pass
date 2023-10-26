6c1db0d0276fab9a061a286975e1742ac9756f68
Gustavo.R <grosario015@gmail.com>
2023-10-25 20:18:05 -0400

    Change name
    Change function name.

--- pj/aplication/views.py
+++ pj/aplication/views.py
@@ -4,11 +4,11 @@ from aplication.controles import turn, record
 from aplication.dto.dto_record import dto_record
 from aplication.dto.dto_generated_turn import dto_generated_turn
 
-def turn(request):
+def next(request):
     
     _record = dto_record() #Objeto Historial
     _generated_turnn = dto_generated_turn()
-    _turno = turn.generate(1)
+    _turno = turn.generate(2)
     _record.turn = _turno
     _generated_turnn.turn = _turno
     turn.register(_generated_turnn)
